cb6c5e9a9824be57802f35b2f5b933d0b3ea6cbe
Your Name <you@example.com>
2023-05-15 18:17:20 +0545

    Addition of gpt Joke command is removed When mentioning and asking qsn it replies with ans made by gpt-3.5-turbo

---
+++ src/events/client/mentionHandler.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+// Connecting to OpenAI
+const configuration = new Configuration({
+  organization: process.env.ORGANIZATION,
+  apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// List of responses when content is empty
+const botResponses = [
+  "Greetings! How may I assist you today?",
+  "Hello there! How can I help you?",
+  "Ah, you've summoned me! What can I do for you?",
+  "At your service! How may I be of assistance?",
+  "I'm here! What would you like to know?",
+  "Hello! How can I assist you today?",
+  "You called? How may I help you?",
+  "Greetings, human! What do you need from me?",
+  "Hello, how can I be of service to you?",
+  "I'm here and ready to assist! What can I do for you?",
+];
+
+async function handleMention(message) {
+  // parse content after @CCRC IT CLUB
+  const content = message.content.replace(/<@!?\d+>/g, "").trim();
+
+  if (content.length == 0)
+    return message.channel.send(botResponses[Math.floor(Math.random() * botResponses.length)]);
+
+  // Sends request to OpenAI
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: content }],
+  });
+
+  await message.channel.sendTyping();
+  await message.channel.send(completion.data.choices[0].message);
+}
+
+module.exports = { handleMention };
--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,20 +1,11 @@
 const axios = require("axios");
+const { handleMention } = require("./mentionHandler");
 
 module.exports = {
   name: "messageCreate",
   async execute(message, client) {
     if (message.author.bot) return;
-    if (message.mentions.has(client.user)) {
-      axios
-        .get("https://official-joke-api.appspot.com/random_joke")
-        .then((response) => {
-          const data = response.data;
-          message.channel.send(`${data.setup}\n\n${data.punchline}`);
-        })
-        .catch((error) => {
-          console.log("Error:", error);
-        });
-    }
+    if (message.mentions.has(client.user)) handleMention(message);
     if (message.channelId != "1029378762707574864") return;
     return message.react("<:Hi_Badge:1030049540516085912>");
   },
